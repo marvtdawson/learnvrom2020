cbd21dcb8f140438053e36425cce1864d057edfe
Marvin Dawson <marvintdawson@gmail.com>
2020-05-17 22:05:33 -0500

    added font awesome link and favicon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+// tslint:disable-next-line:component-class-suffix
 export class HomePage {
 
+  headerLogo = '/assets/imgs/logo.png';
+  // homePageImage = '/assets/imgs/site/front_office2.jpg';
+  customerSatisfactionImage = '/assets/imgs/site/customer_satisfaction_2.png';
+  // siteName = 'VROM';
+  siteFooterName = 'VROM LLC';
+  // siteTagLine = 'Manage Anywhere...';
+  valueProposition = 'Capture Data Build Perception Increase Profits';
+  valuePropositionFooter = 'Capture Data -> Build Perception -> Increase Profits';
+  date = new Date().getFullYear();
   constructor() {}
+
+  openFMLModal() {}
 
 }
